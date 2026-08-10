b4d0a9675fba3e04940947716360f7f5aaf5b338
Rahees Ahmad <ahmadrahees156@gmail.com>
2026-08-10 10:04:11 +0530

    Time: 0 ms (100%), Space: 9 MB (8.82%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+
+    int lengthOfLastWord(string s) {
+      reverse(s.begin(),s.end());
+      int i=0;
+      while(i<s.length()&&s[i]==' ') i++;
+       int count=0;
+    while(i<s.length()&&s[i]!=' '){
+        i++;
+        count++;
+    }
+        return count;
+    }
+};
