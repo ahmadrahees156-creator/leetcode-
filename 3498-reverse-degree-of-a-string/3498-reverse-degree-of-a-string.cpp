d027b78226e5510a1f5977d1979ab94c86e92b30
Rahees Ahmad <ahmadrahees156@gmail.com>
2026-09-07 23:53:06 +0530

    Time: 3 ms (16.9%), Space: 9.7 MB (47.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,8 +3,8 @@ public:
     int reverseDegree(string s) {
         int ans = 0;
         for (int i = 0; i < s.size(); i++) {
-            int value = 'z' - s[i] + 1;
-            ans += value * (i + 1);
+
+            ans += ('z' - s[i] + 1)* (i + 1);
         }
         return ans;
     }
